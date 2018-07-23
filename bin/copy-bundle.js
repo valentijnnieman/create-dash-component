f9e87e0685ae3f07808ca3442173c59095c1a0b5
Valentijn Nieman <valentijnnieman@gmail.com>
2018-07-23 14:13:25 +0200

    Refactor for testing

--- bin/copy-bundle.js
+++ bin/copy-bundle.js
@@ -1,37 +1,44 @@
 #! /usr/bin/env node
 
-const shell = require('shelljs');
-const find = require('find');
-const cp = require('cp');
+const shell = require("shelljs");
+const chalk = require("chalk");
+const find = require("find");
+const cp = require("cp");
+// const args = process.argv.slice(2);
 
-// Note that create-dash-component currently doesn't pass in
-// args to dash-copy-bundle, so it just uses the default build/static/js
-// as used in create-react-app
-const args = process.argv.slice(2);
+// const inputFolder = args[0] || "./build/static/js/";
+// const fileName = args[1] || /\.js$/;
 
-const package = require(process.cwd() + '/package.json');
+function copyBundle(inputFolder = "./build/static/js/", fileName = /\.js$/) {
+  console.log("Looking for build file in: " + inputFolder);
 
-const inputFolder = args[0] || "./build/static/js/"
-const outputFolder = package.name.replace('-', '_'); // replace - with _ so python can read it 
-const fileName = args[1] || /\.js$/
+  const package = require(process.cwd() + "/package.json");
+  const outputFolder = package.name.replace("-", "_"); // replace - with _ so python can read it
 
-console.log("Looking for build file in: " + inputFolder)
+  try {
+    const files = find.fileSync(fileName, inputFolder);
+    console.log(`Found ${chalk.yellow(files)}, copying...`);
+    shell.exec(`cp ${files[0]} ./${outputFolder}/bundle.js`);
+    console.log(
+      `Copied ${chalk.yellow(files)} as ${chalk.magenta(
+        "bundle.js"
+      )} into ${outputFolder}`
+    );
+    shell.exec(
+      `cp ${process.cwd()}/package.json ./${outputFolder}/package.json`
+    );
+    console.log(`Copied ${chalk.yellow("package.json")} into ${outputFolder}`);
 
-find.file(fileName, inputFolder, files => {
-  console.log(`Found ${files}, copying...`)
-  shell.exec(`cp ${files[0]} ./${outputFolder}/bundle.js`)
-  console.log(`Copied ${files} as bundle.js into ${outputFolder}`)
-  shell.exec(`cp ${process.cwd()}/package.json ./${outputFolder}/package.json`)
-  console.log("Copied package.json into " + outputFolder)
+    cp.sync(__dirname + "/init-file.py", outputFolder + "/__init__.py");
+    console.log(`Created ${chalk.yellowBright('__init__.py')} file in ${outputFolder}`);
 
-  console.log("Creating __init__.py file...")
-  cp.sync(__dirname + '/init-file.py', outputFolder + '/__init__.py');
-  console.log('Created __init__.py file in ' + outputFolder)
+    cp.sync(__dirname + "/init-setup.py", "setup.py");
+    console.log(`Created ${chalk.yellowBright('setup.py')} file in ${outputFolder}`);
+  } catch (error) {
+    const buildErrorMsg =
+      "Couldn't find build file! Are you sure you pointed to the right location? In create-react-app, this would be build/static/js\n\n";
+    throw new Error(buildErrorMsg);
+  }
+}
 
-  console.log("Creating setup.py file...")
-  cp.sync(__dirname + '/init-setup.py', 'setup.py');
-  console.log('Created setup.py file in ' + outputFolder)
-}).error(err => {
-  console.log(err)
-})
-
+module.exports = copyBundle;
--- bin/main.js
+++ bin/main.js
@@ -1,25 +1,58 @@
 #! /usr/bin/env node
 
-const shell = require('shelljs');
+const chalk = require('chalk');
+const copyBundle = require('./copy-bundle')
+const extractMeta = require('./extract-meta')
 
 const args = process.argv.slice(2);
 
 if(args.length < 2) {
   console.error("Please specify path to components you want to transform, and the location of your build folder, i.e. create-dash-component src/components build")
+  process.exit(1);
 } else {
   const componentPath = args[0];
   const buildFileLocation = args[1];
-  console.log("=========================");
+
+  introMessage();
+  // try {
+  //   shell.exec("dash-extract-meta " + componentPath);
+  // } catch(error) {
+  //   console.log(error);
+  //   process.exit(1);
+  // }
+
+  createDashComponent();
+
+}
+
+function createDashComponent() {
+  const componentPath = args[0];
+  const buildFileLocation = args[1];
+  try {
+    extractMeta(componentPath)
+  } catch(error) {
+    console.log(chalk.red('\n', error))
+    process.exit(1)
+  }
+  try {
+    copyBundle(buildFileLocation);
+  } catch(error) {
+    console.log(chalk.red('\n', error))
+    process.exit(1)
+  }
+
+  successMessage();
+}
+
+function introMessage() {
+  console.log("\n-------------------------")
   console.log("| create-dash-component |");
-  console.log("=========================");
+  console.log("-------------------------\n");
+}
 
-  shell.exec("dash-extract-meta " + componentPath);
-
-  shell.exec("dash-copy-bundle " + buildFileLocation)
-  console.log("==========================");
-  console.log("| Success! Install your   |");
-  console.log("| new dash component by   |");
-  console.log("| running:                |");
-  console.log("| python setup.py install |");
-  console.log("==========================");
+function successMessage() {
+  console.log("\n");
+  console.log(chalk.greenBright("Success! Install your new Dash component by running: "));
+  console.log(chalk.yellowBright("python setup.py install"));
+  console.log(chalk.greenBright("in your terminal!"));
 }
